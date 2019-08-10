c1bf3de53545a59753fefc8e8b4016080c8b3e44
Christian Onuk <opiskull@Momos.local>
2019-08-10 23:24:48 +0200

    style(app): fix style for app

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import { Router, RouterConfiguration, activationStrategy } from "aurelia-router"
 @autoinject
 export class App {
   public message: string = 'Hello App!';
-
   public router: Router;
 
   constructor(private aurelia: Aurelia) {
@@ -15,8 +14,14 @@ export class App {
 
   configureRouter(config: RouterConfiguration, router: Router) {
     config.map([
-      { route: ['', 'test-route-1'], moduleId: PLATFORM.moduleName('./routes/test-route-1'), nav: false, title: 'TestRoute1', activationStrategy: activationStrategy.replace },
-      { route: 'test-route-2', moduleId: PLATFORM.moduleName('./routes/test-route-2'), nav: false, title: 'TestRoute2', activationStrategy: activationStrategy.replace },
+      {
+        route: ['', 'test-route-1'], moduleId: PLATFORM.moduleName('./routes/test-route-1'),
+        nav: false, title: 'TestRoute1', activationStrategy: activationStrategy.replace
+      },
+      {
+        route: 'test-route-2', moduleId: PLATFORM.moduleName('./routes/test-route-2'),
+        nav: false, title: 'TestRoute2', activationStrategy: activationStrategy.replace
+      },
     ]);
     this.router = router;
   }
